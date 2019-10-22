0379c8e0176e072cb716e1ea32c837786ebbdc28
knobz12 <mjojla@hotmail.com>
2019-10-22 17:08:06 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+"""
+This is configurations for the production server.
+Please don't break it, use test configs first
+"""
+
---
+++ instance/config.py
@@ -0,0 +1,7 @@
+"""
+This file contains configurations for your own machine.
+It will be run after the normal config and overwrite its settings.
+Changes in this file won't break the production version.
+"""
+DEBUG       = True
+SECRET_KEY  = 'h4shiscoolkids'
